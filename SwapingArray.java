93c1b311d42de889bbb370a709a95690cd6c5d9d
madhavi0422 <madhupolu365@gmail.com>
2025-06-17 22:42:47 +0530

    Add files via upload

---
+++ SwapingArray.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class SwapingArray
+{
+	public static void main(String[] args) {
+	    int a=6,b=9;
+	    int temp=0;
+	    temp=a;
+	    a=b;
+	    b=temp;
+	    System.out.println(a+" "+b);
+	}
+}
