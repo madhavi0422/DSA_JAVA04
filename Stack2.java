d9112436018fcfb213541e53711e49a7e89859cb
madhavi0422 <madhupolu365@gmail.com>
2025-06-17 22:29:41 +0530

    Add files via upload

---
+++ Stack2.java
@@ -0,0 +1,13 @@
+
+import java.util.Stack;
+public class Stack2
+{
+	public static void main(String[] args) {
+	    Stack s=new Stack<>();
+	    s.push(1);
+	    s.push(2);
+	    s.push(3);
+	   System.out.println(s.pop());
+	   System.out.println(s.peek());
+	}
+}
